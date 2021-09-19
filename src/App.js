9ec171fae7c25ad6bfde13b64d7157ca21a65169
4dambarr <ab2474@york.ac.uk>
2021-09-19 15:49:23 +0100

    Setup site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+
+const PageHolder = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const Link = styled.a`
+ text-decoration: none;
+ font-size: x-large;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageHolder>
+      <h1>{"<Yordevs />"}</h1>
+      <Link href="https://yordevs.com">Website</Link>
+      <Link href="https://discord.gg/ywfNpWFQk3">Discord</Link>
+      <Link href="mailto:yordevs@yusu.org">Email</Link>
+      <Link href="https://facebook.com/yordevs">Facebook</Link>
+      <Link href="https://twitter.com/yordevs">Twitter</Link>
+      <Link href="https://instagram.com/yordevs">Instagram</Link>
+    </PageHolder>
   );
 }
 
